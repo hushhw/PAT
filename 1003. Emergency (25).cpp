bd582ce079995d062b2e55fe339a5137635b99eb
hushhw <hushhw@sina.cn>
2018-01-22 23:50:30 +0800

    update

--- 1003. Emergency (25).cpp
+++ 1003. Emergency (25).cpp
@@ -48,6 +48,10 @@ n个城市m条路，每个城市有救援小组，所有的边的边权已知。
 如果有多条就输出点权（城市救援小组数目）最大的那个
 */
 
+
+/*
+dfs解法
+
 #include <cstdio>
 #include <iostream>
 #include <string>
@@ -55,7 +59,31 @@ using namespace std;
 
 int n, m, c1, c2;
 int final_len = 10000, final_team, routes;
-int isVisited[500], teams[500], cities[500][500];
+int isVisited[500];		//标记数组
+int teams[500];			//存储每个城市救援队数
+int cities[500][500];	//存储两个城市之间路径
+
+void dfs(int cur, int dest, int len, int team, int n)
+{
+	if(cur == dest){
+		if(final_len > len){
+			final_len = len;
+			routes = 1;
+			final_team = team;
+		} else if(final_len == len){
+			routes += 1;
+			final_team = (final_team > team ? final_team : team);
+		}
+		return ;
+	}
+	for(int i=0; i<n; i++){
+		if(!isVisited[i] && cities[cur][i] != 0){
+			isVisited[i] = 1;
+			dfs(i, dest, len+cities[cur][i], team + teams[i], n);
+			isVisited[i] = 0;
+		}
+	}
+}
 
 int main()
 {
@@ -65,10 +93,80 @@ int main()
 			cin>>teams[i];
 		}
 		for(int i=0; i<m; i++){
-
+			int a = 0, b = 0, l = 0;
+			cin>>a>>b>>l;
+			cities[a][b] = cities[b][a] = l;
 		}
+
+		isVisited[c1] = 1;
+		dfs(c1, c2, 0, teams[c1], n);
+		cout<<routes<<" "<<final_team<<endl;
 	}
 
 	return 0;
 }
 
+*/
+//Dijkstra算法
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int n, m, c1, c2;
+int e[510][510];	//存储路径
+int weight[510];	//存储每个位置救援队数
+int dis[510];		//存储出发点到i结点路径长度
+int num[510];		//最短路径条数
+int w[510];			//救援队数目之和
+bool visit[510];
+const int inf = 99999999;
+
+int main(){
+	while(cin>>n>>m>>c1>>c2){
+		for(int i=0; i<n; i++){
+			cin>>weight[i];
+		}
+		fill(e[0], e[0]+510*510, inf);
+		fill(dis, dis+510, inf);
+		int a, b, c;
+		for(int i = 0; i < m; i++) {
+			cin>>a>>b>>c;
+			e[a][b] = e[b][a] = c;
+		}
+		dis[c1] = 0;
+		w[c1] = weight[c1];
+		num[c1] = 1;
+		for(int i=0; i<n ;i++){
+			int u = -1, minn = inf;
+			cout<<"-------------"<<endl;
+			for(int j=0; j<n; j++){
+				if(visit[j] == false && dis[j]<minn){
+					u = j;
+					
+					minn = dis[j];
+					cout<<"minn:"<<minn<<endl;
+				}
+			}
+			if(u == -1) break;
+			visit[u] = true;
+			for(int v=0; v<n; v++){
+				if(visit[v] == false && e[u][v] != inf){
+					if(dis[u]+e[u][v] < dis[v]){
+						dis[v] = dis[u] + e[u][v];
+						num[v] = num[u];
+						w[v] = w[u] + weight[v];
+					}else if(dis[u]+e[u][v] == dis[v]){
+						num[v] = num[v] + num[u];
+						if(w[u] + weight[v] > w[v])
+							w[v] = w[u] + weight[v];
+					}
+					cout<<"dis[v]:"<<dis[v]<<" v:"<<v<<endl;
+				}
+			}
+		}
+		cout<<num[c2]<<" "<<w[c2]<<endl;
+	}
+	return 0;
+}
---
+++ 1004. Counting Leaves (30).cpp
@@ -0,0 +1,112 @@
+/*
+1004. Counting Leaves (30)
+
+A family hierarchy is usually presented by a pedigree tree. 
+Your job is to count those family members who have no child.
+Input
+
+Each input file contains one test case. Each case starts with a line containing 0 < N < 100, the number of nodes in a tree, and M (< N), the number of non-leaf nodes. Then M lines follow, each in the format:
+
+ID K ID[1] ID[2] ... ID[K]
+where ID is a two-digit number representing a given non-leaf node, K is the number of its children, followed by a sequence of two-digit ID's of its children. For the sake of simplicity, let us fix the root ID to be 01.
+Output
+
+For each test case, you are supposed to count those family members who have no child for every seniority level starting from the root. The numbers must be printed in a line, separated by a space, and there must be no extra space at the end of each line.
+
+The sample case represents a tree with only 2 nodes, where 01 is the root and 02 is its only child. Hence on the root 01 level, there is 0 leaf node; and on the next level, there is 1 leaf node. Then we should output "0 1" in a line.
+
+Sample Input
+2 1
+01 1 02
+Sample Output
+0 1
+*/
+
+//dfs
+/*
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+vector<int> v[100];
+int book[100], maxdepth = -1;
+
+void dfs(int index, int depth){
+	if(v[index].size() == 0){
+		book[depth]++;
+		maxdepth = max(maxdepth, depth);
+		return ;
+	}
+	for(int i=0; i < (v[index].size()); i++)
+		dfs(v[index][i], depth+1);
+}
+
+
+int main(){
+	int n, k, m, node, c;
+	cin>>n>>m;//n结点总数，m非叶子节点数
+	for(int i=0; i<m; i++){
+		cin>>node>>k; //子节点数
+		for(int j=0; j<k; j++){
+			cin>>c;
+			v[node].push_back(c);
+		}
+	}
+	dfs(1, 0);
+	cout<<book[0];
+	for(int i=1; i<=maxdepth; i++)
+		cout<<" "<<book[i];
+	cout<<endl;
+	system("pause");
+	return 0;
+}
+*/
+
+//bfs
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <vector>
+using namespace std;
+int level[100], book[100], maxlevel = -1;
+vector<int> v[100];
+
+void bfs(){
+	queue<int> q;
+	q.push(1);
+	level[1] = 0;
+	while(!q.empty()){
+		int index = q.front();
+		q.pop();
+		maxlevel = max(level[index], maxlevel);
+		if(v[index].size() == 0)
+			book[level[index]]++;
+		for(int i=0; i<v[index].size(); i++){
+			q.push(v[index][i]);
+			level[v[index][i]] = level[index]+1;
+		}
+	}
+}
+
+int main(){
+	int n, k, m, node, c;
+	cin>>n>>m;//n结点总数，m非叶子节点数
+	for(int i=0; i<m; i++){
+		cin>>node>>k; //子节点数
+		for(int j=0; j<k; j++){
+			cin>>c;
+			v[node].push_back(c);
+		}
+	}
+	bfs();
+	printf("%d", book[0]);
+	for(int i = 1; i <= maxlevel; i++)
+		printf(" %d", book[i]);
+	system("pause");
+	return 0;
+}
