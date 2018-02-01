ac9254615607975c50df4ddc65339709559c3186
hushhw <hushhw@sina.cn>
2018-02-01 16:14:43 +0800

    update 素数

---
+++ PAT乙级/1013. 数素数 (20).cpp
@@ -0,0 +1,54 @@
+/*
+1013. 数素数 (20)
+
+令Pi表示第i个素数。现任给两个正整数M <= N <= 104，请输出PM到PN的所有素数。
+
+输入格式：
+
+输入在一行中给出M和N，其间以空格分隔。
+
+输出格式：
+
+输出从PM到PN的所有素数，每10个数字占1行，其间以空格分隔，但行末不得有多余空格。
+
+输入样例：
+5 27
+输出样例：
+11 13 17 19 23 29 31 37 41 43
+47 53 59 61 67 71 73 79 83 89
+97 101 103
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool isprime(int a) {
+	for (int i = 2; i * i <= a; i++)
+		if(a % i == 0) return false;
+	return true;
+}
+int main() {
+	int M, N, num = 2, cnt = 0;
+	cin >> M >> N;
+	vector<int> v;
+	while (cnt < N) {
+		if (isprime(num)) {
+			cnt++;
+			if (cnt >= M) v.push_back(num);
+		}
+		num++;
+	}
+	cnt = 0;
+	for (int i = 0; i < v.size(); i++) {
+		cnt++;
+		if (cnt % 10 != 1) printf(" ");
+		printf("%d", v[i]);
+		if (cnt % 10 == 0) printf("\n");
+	}
+	system("pause");
+	return 0;
+}
