59bfb07c792e12a1d83b9b69ef6858d28427da01
hushhw <hushhw@sina.cn>
2018-02-01 16:14:30 +0800

    update

--- PAT乙级/1012. 数字分类 (20).cpp
+++ PAT乙级/1012. 数字分类 (20).cpp
@@ -43,7 +43,7 @@ int main(){
 			cin>>a[i];
 		int a1=0,a2=0,a3=0,a5=0;
 		double a4=0.0;
-		bool key = true;
+		bool key = true,a2_key=false;
 		int a4_num=0;
 
 		for(int i=0; i<N; i++){
@@ -53,6 +53,7 @@ int main(){
 
 			if(a[i]%5==1){
 				if(key){
+					a2_key = true;
 					a2 = a2 + a[i];
 					key = false;
 				}else {
@@ -81,7 +82,8 @@ int main(){
 				else cout<<a1<<" ";
 			}
 			if(i==1){
-				if(a2==0)	cout<<"N ";
+				if(a2==0 && a2_key)	cout<<"0 ";
+				else if(a2==0 && !a2_key)	cout<<"N ";
 				else cout<<a2<<" ";
 			}
 			if(i==2){
@@ -104,3 +106,37 @@ int main(){
 	}
 	return 0;
 }
+
+/*
+int main() {
+	int n, num, A1 = 0, A2 = 0, A5 = 0;
+	double A4 = 0.0;
+	cin >> n;
+	vector<int> v[5];
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		v[num%5].push_back(num);
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < v[i].size(); j++) {
+			if (i == 0 && v[i][j] % 2 == 0) A1 += v[i][j];
+			if (i == 1 && j % 2 == 0) A2 += v[i][j];
+			if (i == 1 && j % 2 == 1) A2 -= v[i][j];
+			if (i == 3) A4 += v[i][j];
+			if (i == 4 && v[i][j] > A5) A5 = v[i][j];
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		if (i != 0) printf(" ");
+		if (i == 0 && A1 == 0 || i != 0 && v[i].size() == 0) {
+			printf("N"); continue;
+		}
+		if (i == 0) printf("%d", A1);
+		if (i == 1) printf("%d", A2);
+		if (i == 2) printf("%d", v[2].size());
+		if (i == 3) printf("%.1f", A4 / v[3].size());
+		if (i == 4) printf("%d", A5);
+	}
+	return 0;
+}
+*/
